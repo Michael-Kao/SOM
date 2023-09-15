cfe7a455f5e66f5bef037ce582bf81dcc392c240
Kao <music0227@gmail.com>
2023-09-15 16:25:54 +0800

    test

---
+++ out/build/x64-debug/theSOM/shaders/vert.glsl
@@ -0,0 +1,13 @@
+#version 330
+layout (location = 0) in vec3 POSITION;
+layout (location = 1) in vec3 COLOR;
+
+uniform mat4 MODELVIEWPROJECTION;
+
+out vec3 Color;
+
+void main()
+{
+    gl_Position = MODELVIEWPROJECTION * vec4(POSITION, 1.0f);
+    Color = COLOR;
+}
